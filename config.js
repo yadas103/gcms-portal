515b4cc0701bda04247b2ad9eef7fa273abc24d8
PS43 <Selvakumar.P2@pfizer.com>
2017-09-12 17:26:17 +0530

    Configured GCMS service Path

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 angular.module("gcms.environment", [])
-.constant("ENVIRONMENT", {"SERVICE_URI":"http://localhost:8090/GCMSService/","SERVICE_EXT":""});
+.constant("ENVIRONMENT", {"SERVICE_URI":"http://localhost:8090/gcms-service/","SERVICE_EXT":""});

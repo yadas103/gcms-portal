b77556dd75f55f0ae9a3036fb746ff39c0e63618
Khans129 <SelimAktar.Khan@pfizer.com>
2017-09-19 17:46:34 +0530

    Change-Id: I8ebb9961d419ae95cf980870de811b4c1d04c983

--- app/task/task.module.js
+++ app/task/task.module.js
@@ -14,6 +14,7 @@
     .module('gcms.task', [
                           'ui.bootstrap',
                           'gcms.components.data',
-                          'smart-table'                        
+                          'smart-table',
+                          'angularFileUpload'
                           ]);
 }());

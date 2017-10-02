23d85228b77aaae36f53952d1165ec4b4ea956b5
Khans129 <SelimAktar.Khan@pfizer.com>
2017-10-02 17:57:15 +0530

    Change-Id: I6afadb769c1178425c0b47448a1971d73e259a10

--- app/components/modal/controllers/modal.default.controller.js
+++ app/components/modal/controllers/modal.default.controller.js
@@ -12,7 +12,7 @@
     .module('gcms.components.modal')
     .controller('ModalDefaultCtrl', ModalDefault);
 
-    ModalDefault.$inject = ['$scope', '$modalInstance', 'item'];
+    ModalDefault.$inject = ['$scope','$modalInstance', 'item'];
 
     /**
      * @ngdoc method
@@ -23,10 +23,16 @@
      * @param {object} $modalInstance An object which represents a modal window instance
      * @param {object} item The object received from the modal directive controller
      */
-    function ModalDefault($scope, $modalInstance, item) {
-
+    function ModalDefault($scope,$modalInstance, item) {
+    	
       $scope.item = item;
       $scope.maxLength = 100;
+      
+      
+      
+     /* var date=new Date();
+     // $filter('date')(new Date(), 'dd/MM/yyyy');
+      item.date = $filter('date')(date[ item.date, "dd/MM/yyyy"]);*/
       
       //hide popovers in case we came from there
       //$('.popover-link').popover('hide');
@@ -43,148 +49,8 @@
         $scope.item  = (typeof item === 'undefined') ? $scope.item : item;
         $modalInstance.close($scope.item);
       };
-	  $scope.add = function (newValue) {
-        var obj = {};
-        obj.Name = newValue;
-        obj.Value = newValue;
-        $scope.Groups.push(obj);
-        $scope.group.name = obj;
-        $scope.newValue = '';
-    }
 
-    $scope.Groups = [{
-        Name: 'Wrong name selected',
-        Value: 'd1'
-    }, {
-        Name: 'Event canceled',
-        Value: 'A2'
-    }, {
-        Name: 'No payment',
-        Value: 'c3'
-    },   {
-        Name: 'Other reason',
-        Value: 'new'
-    }];
-    $scope.group = {
-        name: ""
-    }
-	$scope.add = function (new1Value) {
-        var obj = {};
-        obj.Name = new1Value;
-        obj.Value = new1Value;
-        $scope.Groupss.push(obj);
-        $scope.group1.name = obj;
-        $scope.new1Value = '';
-    }
-
-    $scope.Groupss = [{
-        Name: 'HCP',
-        Value: 'd1'
-    },   {
-        Name: 'HCO',
-        Value: 'new1'
-    }];
-    $scope.group1 = {
-        name: ""
-    }
-	 $scope.tableRowExpanded = false;
-    $scope.tableRowIndexCurrExpanded = "";
-    $scope.tableRowIndexPrevExpanded = "";
-    $scope.storeIdExpanded = "";
-    $scope.dayDataCollapse = [];
-   
-
-    $scope.dayDataCollapseFn = function () {
-        for (var i = 0; $scope.dataModel.ConsentAnnex.length - 1; i += 1) {
-            $scope.dayDataCollapse.append('true');
-            
-        }
-    };
-    
-  
-    
-    $scope.selectTableRow = function (index, storeId) {
-      
-      
-
-      if ($scope.dayDataCollapse === 'undefined') {
-            $scope.dayDataCollapse = $scope.dayDataCollapseFn();
-            
-      } else {
-    
-    if ($scope.tableRowExpanded === false && $scope.tableRowIndexCurrExpanded === "" && $scope.storeIdExpanded === "") {
-                $scope.tableRowIndexPrevExpanded = "";
-                $scope.tableRowExpanded = true;
-                $scope.tableRowIndexCurrExpanded = index;
-                $scope.storeIdExpanded = storeId;
-                $scope.dayDataCollapse[index] = true;
-    
-    }else if ($scope.tableRowExpanded === true) {
-      if ($scope.tableRowIndexCurrExpanded === index && $scope.storeIdExpanded === storeId) {
-        
-                 $scope.tableRowExpanded = false;
-                    $scope.tableRowIndexCurrExpanded = "";
-                    $scope.storeIdExpanded = "";
-                    $scope.dayDataCollapse[index] = false;
-     
-       }else{
-         
-                 $scope.tableRowIndexPrevExpanded = $scope.tableRowIndexCurrExpanded;
-                    $scope.tableRowIndexCurrExpanded = index;
-                    $scope.storeIdExpanded = storeId;
-                    $scope.dayDataCollapse[$scope.tableRowIndexPrevExpanded] = false;
-                    $scope.dayDataCollapse[$scope.tableRowIndexCurrExpanded] = true;
-         
-         
-       }
-     
-      
-    }
-        
-      }  
-    };
-    
-    
-    $scope.profileModle={
-      
-    "BusinessProfile" : [
-{   "Tname": "123",
-"DR": "xyz",
-"QRcode": "231",
-"conpage": "",
-"Tcode": "",
-
-},
-{   
-"Tname": "234",
-"DR": "Consent Form",
-"QRcode": "23",
-"conpage": "",
-"Tcode": "",
-
-},
-{   
-"Tname": "369",
-"DR": "BG",
-"QRcode": "123",
-"conpage": "",
-"Tcode": "",
-
-},
-]
-      
-    };
-    
-    
-  
-  
-
-  
-  
-
-
-
-      /**
+     /**
        * @ngdoc method
        * @name cancel
        * @methodOf gcms.components.modal.controller:ModalDefaultCtrl

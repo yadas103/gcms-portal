7fdb42db0a04fc5865552c4e9fea09e5d5494544
sulochana <sulochana.saran@pfizer.com>
2017-10-27 14:43:44 +0530

    changes for business.profile

--- app/components/data/components.data.service.business.profile.js
+++ app/components/data/components.data.service.business.profile.js
@@ -6,7 +6,17 @@
     .factory('ValidatedProfile', ValidatedProfile);
 
   ValidatedProfile.$inject = ['$resource','localeMapper', 'ENVIRONMENT'];
-  
+  /**
+   * @ngdoc method
+   * @name business.profile
+   * @methodOf gcms.components.data.service:business.profile
+   * @description Constructor for the business profile data service
+   * @param {object} $resource A factory which creates a resource object
+      that lets you interact with RESTful server-side data sources
+   * @param {object} ENVIRONMENT The configuration object which supplies a
+      consistent service uri to use across the application
+   * @returns {object} The business profile data service
+   */
   function ValidatedProfile($resource,localeMapper, ENVIRONMENT) {
 
 	return $resource(

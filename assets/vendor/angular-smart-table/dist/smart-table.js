439355836f359cc1bcb3d3f9f122c596590eceb3
PS43 <Selvakumar.P2@pfizer.com>
2017-12-18 13:21:24 +0530

    Added reset method in smart table js

--- assets/vendor/angular-smart-table/dist/smart-table.js
+++ assets/vendor/angular-smart-table/dist/smart-table.js
@@ -302,6 +302,38 @@ ng.module('smart-table')
       }
     };
   }]);
+ng.module('smart-table')
+.directive('stReset', function(){
+	   return {
+	            require: '^stTable',
+	            scope: { stReset: "=stReset" },
+	            link: function (scope, element, attr, ctrl) {
+	                
+	                scope.$watch("stReset", function () {
+	                    
+	                    if (scope.stReset) {
+
+	                        // remove local storage
+	                        if (attr.stPersist) {
+	                            localStorage.removeItem(attr.stPersist);
+	                        }
+
+	                        // reset table state
+	                        var tableState = ctrl.tableState();
+	                        tableState.search = {};
+	                        tableState.sort = {};
+	                        ctrl.pipe();
+
+	                        // reset scope value
+	                        scope.stReset = false;
+	                    }
+
+	                });
+	            }
+	        };
+	});
+
+
 
 ng.module('smart-table')
   .directive('stSelectRow', ['stConfig', function (stConfig) {
